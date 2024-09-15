287ba6688ec339eb77abd157eb6c984f0ed2c7da
maichihieu <23730166@ms.uit.edu.vn>
2024-09-15 13:31:14 +0700

    Update InputHandler.java

--- src/InputHandler.java
+++ src/InputHandler.java
@@ -4,6 +4,10 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 public class InputHanler extends KeyAdapter {
-  
+  private ObjectManager = objectManager;
+
+  public InputHandler(ObjectManager objectManager){
+    this.objectManager = objectManager;
+  }
 
 }
