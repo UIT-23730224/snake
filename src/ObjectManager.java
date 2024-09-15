e0f6901eb03dcbac7b55fad0f93f22bdb2bf3f75
maichihieu <23730166@ms.uit.edu.vn>
2024-09-15 13:28:23 +0700

    Update ObjectManager.java

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -1,1 +1,22 @@
+package src;
 
+import java.util.Random;
+
+public class ObjectManager {
+
+
+
+  public void moveSnake(){
+    for (int i = snakeLenght; i > 0; i--){
+      x[i] = x[i-1];
+      y[i] = y[i-1];
+    }
+    switch (direction){
+      case 'U' -> y[0] = y[0] - UNIT_SIZE;
+      case 'D' -> y[0] = y[0] + UNIT_SIZE;
+      case 'L' -> x[0] = x[0] - UNIT_SIZE;
+      case 'R' -> x[0] = x[0] + UNIT_SIZE;       
+    }
+    
+  }
+}
