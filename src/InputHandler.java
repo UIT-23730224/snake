94581c1d8d1e550789f8c525cd2cdb1824468262
maichihieu <23730166@ms.uit.edu.vn>
2024-09-15 13:38:03 +0700

    Update InputHandler.java

--- src/InputHandler.java
+++ src/InputHandler.java
@@ -9,5 +9,29 @@ public class InputHanler extends KeyAdapter {
   public InputHandler(ObjectManager objectManager){
     this.objectManager = objectManager;
   }
-
+  @Override
+  public void keyPressed(KeyEvent e) {
+    switch (e.getKeyCode()) {
+      case KeyEvent.VK_LEFT:
+        if (objectManager.getDirection() != 'R') {
+            objectManager.setDirection('L');
+        }
+        break;
+      case KeyEvent.VK_RIGHT:
+        if (objectManager.getDirection() != 'L') {
+            objectManager.setDirection('R');
+        }
+        break;
+      case KeyEvent.VK_UP:
+        if (objectManager.getDirection() != 'D') {
+            objectManager.setDirection('U');
+        }
+        break;
+      case KeyEvent.VK_DOWN:
+        if (objectManager.getDirection() != 'U') {
+            objectManager.setDirection('D');
+        }
+        break;
+    }
+  }
 }
