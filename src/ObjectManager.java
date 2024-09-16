0d5deafc285172c9755284d4b630ea70914c12ee
maichihieu <23730166@ms.uit.edu.vn>
2024-09-16 16:12:46 +0700

    Update ObjectManager.java

--- src/ObjectManager.java
+++ src/ObjectManager.java
@@ -3,8 +3,23 @@ package src;
 import java.util.Random;
 
 public class ObjectManager {
-
+  private static final int WIDTH = 500;
+  private static final int HEIGHT = 500;
+  private static final int UNIT_SIZE = 20;
+  private static final int NUMBER_OF_UNITS = (WIDTH * HEIGHT) / (UNIT_SIZE * UNIT_SIZE);
+  
+  private final int[] x = new int[NUMBER_OF_UNITS];
+  private final int[] y = new int[NUMBER_OF_UNITS];
+  
+  private int snakeLength;
+  private int foodEaten;
+  private int foodX;
+  private int foodY;
+  private boolean running;
+  private Random random;
   private char direction;
+
+  
 
   public void moveSnake(){
     for (int i = snakeLenght; i > 0; i--){
@@ -29,4 +44,16 @@ public class ObjectManager {
   public void setDirection(char direction) {
     this.direction = direction;
   }
+
+
+
+  public int getSnakeX(int index) { return x[index]; }
+  public int getSnakeY(int index) { return y[index]; }
+  public int getSnakeLength() { return snakeLength; }
+  public int getFoodEaten() { return foodEaten; }
+  public int getFoodX() { return foodX; }
+  public int getFoodY() { return foodY; }
+  public char getDirection() { return direction; }
+  public void setDirection(char direction) { this.direction = direction; }
+  public boolean isGameRunning() { return running; }
 }
